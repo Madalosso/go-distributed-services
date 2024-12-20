d8f29544ba1544aa4a3fe242967d9c1ebc889cc3
Otavio Migliavacca Madalosso <otaviomadalosso@gmail.com>
2024-12-20 09:08:40 -0300

    Start Load Balancing

---
+++ ClientSideServiceDiscovery/go.mod
@@ -0,0 +1,52 @@
+module github.com/madalosso/proglog
+
+go 1.23.2
+
+require (
+	github.com/casbin/casbin v1.9.1
+	github.com/grpc-ecosystem/go-grpc-middleware v1.1.0
+	github.com/hashicorp/raft v1.1.1
+	github.com/hashicorp/raft-boltdb v0.0.0-20241202213821-f9dd2ba30efd
+	github.com/hashicorp/serf v0.8.5
+	github.com/soheilhy/cmux v0.1.5
+	github.com/stretchr/testify v1.4.0
+	github.com/travisjeffery/go-dynaport v1.0.0
+	github.com/tysonmote/gommap v0.0.3
+	go.opencensus.io v0.22.0
+	go.uber.org/zap v1.10.0
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013
+	google.golang.org/grpc v1.32.0
+	google.golang.org/protobuf v1.25.0
+)
+
+require (
+	cloud.google.com/go v0.38.0 // indirect
+	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
+	github.com/armon/go-metrics v0.0.0-20190430140413-ec5e00d3c878 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/protobuf v1.4.1 // indirect
+	github.com/google/btree v0.0.0-20180813153112-4030bb1f1f0c // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-hclog v0.9.1 // indirect
+	github.com/hashicorp/go-immutable-radix v1.0.0 // indirect
+	github.com/hashicorp/go-msgpack v0.5.5 // indirect
+	github.com/hashicorp/go-multierror v1.0.0 // indirect
+	github.com/hashicorp/go-sockaddr v1.0.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.1 // indirect
+	github.com/hashicorp/memberlist v0.1.3 // indirect
+	github.com/miekg/dns v1.0.14 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sean-/seed v0.0.0-20170313163322-e2103e2c3529 // indirect
+	go.etcd.io/bbolt v1.3.5 // indirect
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/net v0.0.0-20201202161906-c7110b5ffcbb // indirect
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45 // indirect
+	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
+	golang.org/x/text v0.3.3 // indirect
+	google.golang.org/appengine v1.5.0 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+)
+
+replace github.com/hashicorp/raft-boltdb => github.com/travisjeffery/raft-boltdb v0.0.0-20201002143322-bc94ee46437b
---
+++ ClientSideServiceDiscovery/internal/server/server_test.go
@@ -0,0 +1,289 @@
+package server
+
+import (
+	"context"
+	"flag"
+	"net"
+	"os"
+	"testing"
+	"time"
+
+	api "github.com/madalosso/proglog/api/v1"
+	"github.com/madalosso/proglog/internal/auth"
+	"github.com/madalosso/proglog/internal/config"
+	"github.com/madalosso/proglog/internal/log"
+	"github.com/stretchr/testify/require"
+	"go.opencensus.io/examples/exporter"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/status"
+)
+
+var debug = flag.Bool("debug", false, "Enable observability for debugging.")
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	if *debug {
+		logger, err := zap.NewDevelopment()
+		if err != nil {
+			panic(err)
+		}
+		zap.ReplaceGlobals(logger)
+	}
+	os.Exit(m.Run())
+}
+
+func TestServer(t *testing.T) {
+	tests := map[string]func(t *testing.T, rootClient api.LogClient, nobodyClient api.LogClient, config *Config){
+		"produce/consume a message to/from the log succeeds": testProduceConsume,
+		"produce/consume stream succeeds":                    testProduceConsumeStream,
+		"consume past log boundary fails":                    testConsumePastBoundary,
+		"unauthorized fails":                                 testUnauthorized,
+	}
+	for scenario, fn := range tests {
+		t.Run(scenario, func(t *testing.T) {
+			rootClient, nobodyClient, config, teardown := setupTest(t, nil)
+			defer teardown()
+			fn(t, rootClient, nobodyClient, config)
+		})
+	}
+}
+
+func setupTest(t *testing.T, fn func(*Config)) (rootClient api.LogClient, nobodyClient api.LogClient, cfg *Config, teardown func()) {
+	t.Helper()
+
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+	require.NoError(t, err)
+
+	newClient := func(crtPath, keyPath string) (*grpc.ClientConn, api.LogClient, []grpc.DialOption) {
+		tlsConfig, err := config.SetupTLSConfig(config.TLSConfig{
+			CertFile: crtPath,
+			KeyFile:  keyPath,
+			CAFile:   config.CAFile,
+			Server:   false,
+		})
+		require.NoError(t, err)
+		tlsCreds := credentials.NewTLS(tlsConfig)
+		opts := []grpc.DialOption{grpc.WithTransportCredentials(tlsCreds)}
+		conn, err := grpc.Dial(l.Addr().String(), opts...)
+		require.NoError(t, err)
+		client := api.NewLogClient(conn)
+		return conn, client, opts
+	}
+
+	var rootConn *grpc.ClientConn
+	rootConn, rootClient, _ = newClient(
+		config.RootClientCertFile,
+		config.RootClientKeyFile,
+	)
+	var nobodyConn *grpc.ClientConn
+	nobodyConn, nobodyClient, _ = newClient(
+		config.NobodyClientCertFile,
+		config.NobodyClientKeyFile,
+	)
+
+	// clientTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+	// 	CertFile: config.ClientCertFile,
+	// 	KeyFile:  config.ClientKeyFile,
+	// 	CAFile:   config.CAFile,
+	// })
+	// require.NoError(t, err)
+
+	// clientCreds := credentials.NewTLS(clientTLSConfig)
+	// cc, err := grpc.NewClient(l.Addr().String(), grpc.WithTransportCredentials(clientCreds))
+	// require.NoError(t, err)
+	// client = api.NewLogClient(cc)
+
+	serverTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CertFile:      config.ServerCertFile,
+		KeyFile:       config.ServerKeyFile,
+		CAFile:        config.CAFile,
+		ServerAddress: l.Addr().String(),
+		Server:        true,
+	})
+	require.NoError(t, err)
+
+	serverCreds := credentials.NewTLS(serverTLSConfig)
+
+	dir := t.TempDir()
+
+	clog, err := log.NewLog(dir, log.Config{})
+	require.NoError(t, err)
+
+	authorizer := auth.New(config.ACLModelFile, config.ACLPolicyFile)
+
+	var telemetryExporter *exporter.LogExporter
+	if *debug {
+		metricsLogFile, err := os.CreateTemp("", "metrics-*.log")
+		require.NoError(t, err)
+		t.Logf("metrics log file: %s", metricsLogFile.Name())
+
+		tracesLogFile, err := os.CreateTemp("", "traces-*.log")
+		require.NoError(t, err)
+		t.Logf("traces log file: %s", tracesLogFile.Name())
+
+		telemetryExporter, err = exporter.NewLogExporter(exporter.Options{
+			MetricsLogFile:    metricsLogFile.Name(),
+			TracesLogFile:     tracesLogFile.Name(),
+			ReportingInterval: time.Second,
+		})
+		require.NoError(t, err)
+		err = telemetryExporter.Start()
+		require.NoError(t, err)
+	}
+
+	cfg = &Config{CommitLog: clog, Authorizer: authorizer}
+	if fn != nil {
+		fn(cfg)
+	}
+	server, err := NewGRPCServer(cfg, grpc.Creds(serverCreds))
+	require.NoError(t, err)
+
+	go func() {
+		server.Serve(l)
+	}()
+
+	// Remove clog? (source material doesn't include it)
+	return rootClient, nobodyClient, cfg, func() {
+		server.Stop()
+		rootConn.Close()
+		nobodyConn.Close()
+		l.Close()
+		clog.Remove()
+		if telemetryExporter != nil {
+			time.Sleep(1500 * time.Millisecond)
+			telemetryExporter.Stop()
+			telemetryExporter.Close()
+		}
+	}
+
+}
+
+func testProduceConsume(t *testing.T, client api.LogClient, _ api.LogClient, config *Config) {
+	ctx := context.Background()
+
+	logRecord := &api.Record{
+		Value: []byte("hello world"),
+	}
+
+	produce, err := client.Produce(
+		ctx,
+		&api.ProduceRequest{
+			Record: logRecord,
+		},
+	)
+	require.NoError(t, err)
+
+	consume, err := client.Consume(ctx, &api.ConsumeRequest{
+		Offset: produce.Offset,
+	})
+	require.NoError(t, err)
+	require.Equal(t, logRecord.Value, consume.Record.Value)
+	require.Equal(t, logRecord.Offset, consume.Record.Offset)
+}
+
+func testConsumePastBoundary(t *testing.T, client api.LogClient, _ api.LogClient, config *Config) {
+	ctx := context.Background()
+	produce, err := client.Produce(ctx, &api.ProduceRequest{
+		Record: &api.Record{
+			Value: []byte("hello world"),
+		},
+	})
+	require.NoError(t, err)
+
+	consume, err := client.Consume(ctx, &api.ConsumeRequest{Offset: produce.Offset + 1})
+	if consume != nil {
+		t.Fatal("consume not nil")
+	}
+	got := status.Code(err)
+	want := status.Code(api.ErrOffsetOutOfRange{}.GRPCStatus().Err())
+	if got != want {
+		t.Fatalf("got err: %v, want: %v", got, want)
+	}
+}
+
+func testProduceConsumeStream(t *testing.T, client api.LogClient, _ api.LogClient, config *Config) {
+	ctx := context.Background()
+
+	records := []*api.Record{
+		{Value: []byte("first message"), Offset: 0},
+		{Value: []byte("second message"), Offset: 1},
+	}
+
+	{
+		stream, err := client.ProduceStream(ctx)
+		require.NoError(t, err)
+
+		for offset, record := range records {
+			err = stream.Send(&api.ProduceRequest{
+				Record: record,
+			})
+			require.NoError(t, err)
+			res, err := stream.Recv() // Q: what is this?
+			//A: blocks until Receive new message from stream
+			require.NoError(t, err)
+			if res.Offset != uint64(offset) {
+				t.Fatalf(
+					"got offset: %d, want:%d",
+					res.Offset,
+					offset,
+				)
+			}
+		}
+	}
+
+	{
+		// Q: Creates a consume stream with an offset (request)...
+		// like: "opens a request stream"
+		// so it can be read later once the response is available?
+		stream, err := client.ConsumeStream(
+			ctx,
+			&api.ConsumeRequest{Offset: 0},
+		)
+		require.NoError(t, err)
+
+		for i, record := range records {
+			res, err := stream.Recv()
+			require.NoError(t, err)
+			require.Equal(t, res.Record, &api.Record{
+				Value:  record.Value,
+				Offset: uint64(i),
+			})
+
+		}
+	}
+}
+
+func testUnauthorized(
+	t *testing.T,
+	_,
+	client api.LogClient,
+	_ *Config,
+) {
+	ctx := context.Background()
+	produce, err := client.Produce(ctx, &api.ProduceRequest{
+		Record: &api.Record{
+			Value: []byte("hello world"),
+		},
+	})
+
+	if produce != nil {
+		t.Fatalf("produce response should be nil")
+	}
+	gotCode, wantCode := status.Code(err), codes.PermissionDenied
+	if gotCode != wantCode {
+		t.Fatalf("got code %d, want %d", gotCode, wantCode)
+	}
+
+	consume, err := client.Consume(ctx, &api.ConsumeRequest{Offset: 0})
+	if consume != nil {
+		t.Fatalf("consume response should be nil")
+	}
+	gotCode, wantCode = status.Code(err), codes.PermissionDenied
+	if gotCode != wantCode {
+		t.Fatalf("got code %d, want %d", gotCode, wantCode)
+	}
+
+}
